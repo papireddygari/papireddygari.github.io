bca7006dc0b39eef8107503416844a0e06391858
papireddygari <69148614+papireddygari@users.noreply.github.com>
2025-09-11 20:10:42 +0000

    Deploying to gh-pages from @ papireddygari/papireddygari.github.io@92ac934a59b807aa1a949e2afa3f6ef29bb525bd 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "papireddygari/maneesha.github.io",
+    "data-repo": "papireddygari/papireddygari.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
