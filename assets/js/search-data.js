23c01396438beccb83c14a5b0d9f7fdba872482d
papireddygari <69148614+papireddygari@users.noreply.github.com>
2025-09-12 02:27:39 +0000

    Deploying to gh-pages from @ papireddygari/papireddygari.github.io@9ded738525588ba503f2e02d881be4faa1692134 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -520,7 +520,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=s0nJaJAAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
