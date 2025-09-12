2dde79156dc1e275c4211c2458fc894e9a3ab52f
papireddygari <69148614+papireddygari@users.noreply.github.com>
2025-09-12 02:35:44 +0000

    Deploying to gh-pages from @ papireddygari/papireddygari.github.io@7648f1865df3412bc43e72f8c33921d42c61c146 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,7 +499,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%61%70%69%72%65%64%64@%62%63.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
