75c14a34182d27edd7b653488c34d77d4ff6f090
papireddygari <69148614+papireddygari@users.noreply.github.com>
2025-09-16 03:07:52 +0000

    Deploying to gh-pages from @ papireddygari/papireddygari.github.io@da2151cefacba73e6c713fda5cac7dd385eeb97a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Heres a brief summary of my CV. See PDF for the full version.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
