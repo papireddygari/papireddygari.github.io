f0ab11b748a4af516664f483712fee1a720ae87a
papireddygari <69148614+papireddygari@users.noreply.github.com>
2025-09-16 01:54:07 +0000

    Deploying to gh-pages from @ papireddygari/papireddygari.github.io@a1dcd2d25bd916c6c9861142b90c49c973e8faec 🚀

--- _pages/teaching.md
+++
@@ -1,9 +0,0 @@
-
----
-layout: page
-title: "Teaching"
-permalink: /teaching/
-author_profile: true
-nav: true
-nav_order: 6
----
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Materials for courses you taught. Replace this text with your description.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "news-i-will-be-spending-my-summer-as-a-research-intern-at-ethereum-foundation",
           title: 'I will be spending my Summer as a research intern at Ethereum Foundation...',
           description: "",
