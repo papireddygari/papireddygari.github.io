dad7d967115f34faed92ee58c5ea6deb6c0f3524
papireddygari <69148614+papireddygari@users.noreply.github.com>
2025-09-16 14:58:15 +0000

    Deploying to gh-pages from @ papireddygari/papireddygari.github.io@c7df7b62f80e7ed07124e531b8360b7103f30f25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-courses",
+          title: "Courses",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
